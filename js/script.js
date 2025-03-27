dd1b87c47c04a0854a87fa329e38abbc27d64a53
simonelupone <simone.lupone@gmail.com>
2025-03-27 16:24:23 +0100

    [feat]: add countdown function with dom elements handling

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 // definisco gli elementi del dom che mi servono
 const countdown = document.getElementById('countdown'); // numero--
 const numberList = document.getElementById('numbers-list'); // lista numeri da far apparire
+const answersForm = document.getElementById('answers-form'); // lista input per risposta
+const instructions = document.getElementById('instructions'); // stringa dinamica
 
 // funzione per generare i numeri random
 const randomNumbers = () => Math.floor(Math.random() * 100) + 1;
@@ -12,3 +14,20 @@ const pickNumbers = (node) => {
     }
 }
 pickNumbers(numberList);
+
+// funzione per effettuare un countdown da 30 secondi
+let count = 3;
+countdown.innerHTML = count--;
+const startGame = setInterval(() => {
+    if (count >= 0) {
+        countdown.innerHTML = count;
+    } else {
+        clearInterval(startGame);
+        numberList.innerHTML = '';
+        answersForm.classList.remove('d-none');
+        instructions.innerHTML = 'Inserisci tutti i numeri che ricordi, (l\'ordine non è importante)';
+    }
+    count--;
+}, 1000);
+
+startGame;
